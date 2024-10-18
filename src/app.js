f6d6a6d81c49916368080a52b926fdf798076744
Nitin Pathak <nitinpathak8242@gmail.com>
2024-10-18 17:10:25 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -3,22 +3,15 @@ const express = require("express");
 const app = express();
 
 // This will only handle the GET call to /user
-app.get("/user", (req, res) => {
+app.get("/user/:userId", (req, res) => {
+    console.log(req.params);
     res.send({firstName: "Nitin", lastName: "Pathak"});
-}); 
-
-app.post("/user", (req, res) => {
-    // saving data to DB
-    res.send("Data successfully saved to the database!");
-}); 
-
-// This will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-    res.send("Hello from the server!!");
 }); 
 
 app.listen(3000, () => {
     console.log("Server is listening successfully on port 3000");
 });
+
+
 
 
