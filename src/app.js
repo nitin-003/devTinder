5ce43e04eba3ef1e51d228a4459b3c418f80e5bb
Nitin Pathak <nitinpathak8242@gmail.com>
2024-10-19 00:45:24 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -1,23 +1,27 @@
 const express = require("express");
 const app = express();
 
-const {adminAuth, userAuth} = require("./middlewares/auth");
-
-// Handle auth middleware for all GET, POST, ... requests
-app.use("/admin", adminAuth);
-
-app.get("/user", userAuth, (req, res) => {
-    res.send("User Data Sent");
+app.get("/getUserData", (req, res) => {
+    try{
+        throw new Error("ddfgh");
+        res.send("User Data Sent");
+    }
+    catch(err){
+        res.status(500).send("Some error contact support team");
+    }
 });
 
-app.get("/admin/getAllData", (req, res) => {
-    // Check if request is authorized
-    res.send("All Data Sent");
+app.use("/", (err, req, res, next) => {
+    if(err){
+        // log your error
+        res.status(500).send("Something went wrong!!");
+    }
 });
 
 app.listen(3000, () => {
     console.log("Server is listening successfully on port 3000");
 });
+
 
 
 
