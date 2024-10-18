8cfd9fca13b6b1f392805dee2e222adba8b0100a
Nitin Pathak <nitinpathak8242@gmail.com>
2024-10-18 16:53:12 +0530

    exploring get, post, etc

--- src/app.js
+++ src/app.js
@@ -2,6 +2,17 @@ const express = require("express");
 
 const app = express();
 
+// This will only handle the GET call to /user
+app.get("/user", (req, res) => {
+    res.send({firstName: "Nitin", lastName: "Pathak"});
+}); 
+
+app.post("/user", (req, res) => {
+    // saving data to DB
+    res.send("Data successfully saved to the database!");
+}); 
+
+// This will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
     res.send("Hello from the server!!");
 }); 
@@ -9,6 +20,5 @@ app.use("/test", (req, res) => {
 app.listen(3000, () => {
     console.log("Server is listening successfully on port 3000");
 });
-
 
 
