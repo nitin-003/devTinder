50a1b6a962416fba3f5e206ab950899bcf2d43cb
Nitin Pathak <nitinpathak8242@gmail.com>
2024-10-19 00:18:34 +0530

    writing auth-middleware

--- src/app.js
+++ src/app.js
@@ -1,35 +1,23 @@
 const express = require("express");
-
 const app = express();
 
-app.use("/user", 
-    (req, res, next) => {
-    // Route handler  
-    // res.send("Route Handler 1");
-    console.log("Handling the route user 1!")
-    next();
-    },
-    (req, res, next) => {
-        console.log("Handling the route user 2!")
-        next();
-       //  res.send("Route Handler 2")
-    },
-    (req, res, next) => {
-        console.log("Handling the route user 3!")
-        next();
-        // res.send("Route Handler 3")
-    },
-    (req, res, next) => {
-        console.log("Handling the route user 4!")
-        // next();
-        res.send("Route Handler 4")
-    }
-);
+const {adminAuth, userAuth} = require("./middlewares/auth");
+
+// Handle auth middleware for all GET, POST, ... requests
+app.use("/admin", adminAuth);
+
+app.get("/user", userAuth, (req, res) => {
+    res.send("User Data Sent");
+});
+
+app.get("/admin/getAllData", (req, res) => {
+    // Check if request is authorized
+    res.send("All Data Sent");
+});
 
 app.listen(3000, () => {
     console.log("Server is listening successfully on port 3000");
 });
-
 
 
 
