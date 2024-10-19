a474b56c517c6b8e7e79390ebc84f0555816b9d9
Nitin Pathak <nitinpathak8242@gmail.com>
2024-10-19 18:33:17 +0530

    exploring validator library

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
     {
@@ -17,10 +18,20 @@ const userSchema = new mongoose.Schema(
             required: true,
             unique: true,
             trim: true,
+            validate(value){
+                if(!validator.isEmail(value)){
+                    throw new Error("Invalid email address: " + value);
+                }
+            },
         },
         password: {
             type: String,
             required: true,
+            validate(value){
+                if(!validator.isStrongPassword(value)){
+                    throw new Error("Enter a Strong Password: " + value);
+                }
+            },
         },
         age: {
             type: Number,
@@ -37,6 +48,11 @@ const userSchema = new mongoose.Schema(
         photoUrl: {
             type: String,
             default: "https://www.pngall.com/wp-content/uploads/5/Profile-Male-Transparent.png",
+            validate(value){
+                if(!validator.isURL(value)){
+                    throw new Error("Invalid photo URL: " + value);
+                }
+            },
         },
         about: {
             type: String,
@@ -53,5 +69,6 @@ const userSchema = new mongoose.Schema(
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
+
 
 
