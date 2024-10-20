878e57ad59a1084de45579d7b968fe1f62bf2150
Nitin Pathak <nitinpathak8242@gmail.com>
2024-10-20 15:58:11 +0530

    exploring auth-middlewares

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const {validateSignUpData} = require("./utils/validation");
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const {userAuth} = require("./middlewares/auth");
 
 app.use(express.json());
 app.use(cookieParser());
@@ -62,25 +63,27 @@ app.post("/login", async (req, res) => {
 });
 
 
-app.get("/profile", async (req, res) => {
+app.get("/profile", userAuth, async (req, res) => {
     try{
-        const cookies = req.cookies;
+        // const cookies = req.cookies;
 
-        const {token} = cookies;
+        // const {token} = cookies;
     
-        if(!token){
-            throw new Error("Invalid Token");
-        }
+        // if(!token){
+        //     throw new Error("Invalid Token");
+        // }
     
-        // Validate my token
-        const decodedMessage = await jwt.verify(token, "DEV@TINDER$790");
+        // // Validate my token
+        // const decodedMessage = await jwt.verify(token, "DEV@TINDER$790");
     
-        const {_id} = decodedMessage;
+        // const {_id} = decodedMessage;
     
-        const user = await User.findById(_id);
-        if(!user){
-            throw new Error("User does not exist");
-        }
+        // const user = await User.findById(_id);
+        // if(!user){
+        //     throw new Error("User does not exist");
+        // }
+
+        const user = req.user;
     
         res.send(user);
     }
