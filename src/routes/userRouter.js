cb04fbd7650bfea738f647a0c47a4e535ca3846d
Nitin Pathak <nitinpathak8242@gmail.com>
2024-10-21 18:49:40 +0530

    user/connections API

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -23,9 +23,32 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     }
 });
 
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try{
+        const loggedInUser = req.user;
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { toUserId: loggedInUser._id, status: "accepted" },
+                { fromUserId: loggedInUser._id, status: "accepted" },
+            ],
+        }).populate("fromUserId", ["firstName", "lastName"]).populate("toUserId", ["firstName", "lastName"]);
+
+        const data = connectionRequests.map((row) => {
+            if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
+                return row.toUserId;
+            }
+            return row.fromUserId;
+        });
+
+        res.json({ data });
+    }
+    catch(err){
+        res.status(400).send({ message: err.message });
+    }
+});
+
 
 module.exports = userRouter;
-
 
 
 
