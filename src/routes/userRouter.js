9535f8500cf4cbe42713d28f06be05c233771d54
Nitin Pathak <nitinpathak8242@gmail.com>
2024-10-21 20:55:09 +0530

    Exploring $nin, $and, etc

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const {userAuth} = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 // Get all the pending connection request for the loggedIn User
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
@@ -47,8 +48,41 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     }
 });
 
+userRouter.get("/feed", userAuth, async (req, res) => {
+    try{
+        const loggedInUser = req.user;
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                {fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id},
+            ],
+        }).select("fromUserId toUserId");
+
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach((req) => {
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+        });
+
+        console.log(hideUsersFromFeed);
+
+        const users = await User.find({
+            $and: [ 
+                { _id: {$nin: Array.from(hideUsersFromFeed)} },
+                { _id: {$ne: loggedInUser._id} },
+            ],
+        }).select("firstName lastName");
+
+        res.send(users);
+    }
+    catch(err){
+        res.status(400).json({ message: err.message });
+    }
+});
+
 
 module.exports = userRouter;
+
 
 
 
